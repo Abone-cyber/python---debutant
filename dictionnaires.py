df212c10079ebfbc882ce4e87d6fb35e6dcddab1
Abone-cyber <patriceabone@gmail.com>
2025-07-16 00:49:35 +0100

    Add files via upload

---
+++ dictionnaires.py
@@ -0,0 +1,24 @@
+dico = {
+    "Nelson": 10,
+    "Mandela": 12,
+    "Junior": 14,
+    "Kevin": 16,
+    "Abraham": 18
+}
+for n in dico:
+    print(n, "a", dico[n], "ans")
+
+nom = input("Veuillez entrez un nom :")
+if nom in dico:
+    print("Vous avez:" + str(dico[nom]) + " ans.")
+else:
+    print("Vous n'etes pas enregistré.")
+
+    produits = {
+        "Phone": 40000,
+        "Desktop": 200000,
+        "Key": 3000,
+        "Microphone": 15000
+    }
+    for p in produits:
+        print("A", p, "costs:", produits[p], "FCFA" )
