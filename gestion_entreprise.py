00ee2b467899650a39edfe0f574118a416d9a2cb
Abone-cyber <patriceabone@gmail.com>
2026-08-29 22:31:06 +0100

    Implement Entreprise class for employee management
    Added an Entreprise class to manage employees, including methods to add, display, search, and remove employees.

--- gestion_entreprise.py
+++ gestion_entreprise.py
@@ -4,6 +4,10 @@ class Employe:
         self.__salaire = salaire
         self.ordinateur = ordinateur
 
+    @property
+    def nom(self):
+        return self.__nom
+
     def afficher_infos(self):
         return f"Nom : {self.__nom} | Salaire : {self.__salaire} FCFA"
     
@@ -35,10 +39,44 @@ class Ordinateur:
         self.marque = marque
         self.processeur = Processeur("Intel", "2,5 GHz")
 
+class Entreprise:
+    def __init__(self, nom):
+        self.nom = nom
+        self.employes = []
+
+    def ajouter_employe(self,employe):
+        self.employes.append(employe)
+
+    def afficher_employes(self):
+        for employe in self.employes:
+            print(employe.afficher_infos())
+
+    def rechercher_employe(self, nom):
+        for employe in self.employes:
+            if nom == employe.nom:
+                return employe
+        return None
+
+    def supprimer_employe(self, nom):
+        for employe in self.employes:
+            if nom == employe.nom:
+                self.employes.remove(employe)
+                return True
+        return False
+
+
+
 pc = Ordinateur("Dell")
 
 dev = Developpeur("Alice", 400000, "Python", pc)
 
-print(dev.ordinateur.marque)
+enterp = Entreprise("CyberTech")
+enterp.ajouter_employe(dev)
 
+if enterp.supprimer_employe("Alice"):
+    print("Employé supprimé.")
+else:
+    print("Employé Introuvable.")
 
+enterp.afficher_employes()
+print(enterp.supprimer_employe("Alice"))
